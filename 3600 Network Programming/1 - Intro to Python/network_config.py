6edcb15039fd368798d2d23b71171bfab40aefb4
Henry Moore <henrypmoore123@gmail.com>
2021-09-04 11:49:26 -0400

    final attempt 1

--- 3600 Network Programming/1 - Intro to Python/network_config.py
+++ 3600 Network Programming/1 - Intro to Python/network_config.py
@@ -1,15 +1,9 @@
-# TODO: Define a variable named 'config' that contains a dictionary with the entries
-#       'packet_length', 'num_links', 'bandwidths', 'distances', 'transmission_speeds', 'processing_delays', and 'average_packet_arrival_rate'
-#       These variables will contain all of the information we need to compute a packets round trip latency through a network.
-#       'packet_length' should store the length of the packet in bytes
-#       'num_links' should store the number of links the packet will pass through
-#       the remaining entries should store lists of length 'num_links', where each entry in the list corresponds with the value associated with that link
 config = {
-        'packet_length' : 10, 
-        'num_links' : 1, 
-        'bandwidths' : [500], 
-        'distances' : [250], 
-        'transmission_speeds' : [200], 
-        'processing_delays' : [1], 
-        'average_packet_arrival_rate' : [1]
+        'packet_length' : 0, 
+        'num_links' : 0, 
+        'bandwidths' : [], 
+        'distances' : [], 
+        'transmission_speeds' : [], 
+        'processing_delays' : [], 
+        'average_packet_arrival_rate' : []
         }
