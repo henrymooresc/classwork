1326c251dc7c6b4d1c0b3f3a8069dd79dbc93c80
henrymooresc <56162021+henrymooresc@users.noreply.github.com>
2021-09-28 13:22:18 -0400

    Delete 3600 Network Programming directory

--- 3600 Network Programming/1 - Intro to Python/network_config.py
+++
@@ -1,9 +0,0 @@
-config = {
-        'packet_length' : 0, 
-        'num_links' : 0, 
-        'bandwidths' : [], 
-        'distances' : [], 
-        'transmission_speeds' : [], 
-        'processing_delays' : [], 
-        'average_packet_arrival_rates' : []
-        }
